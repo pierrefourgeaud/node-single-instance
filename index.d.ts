d2ce26e02c0c8f96a04492f80dae745a5bcd57f6
David Trotz <davidtrotz@olivetree.com>
2020-12-28 12:23:26 -0800

    Proper exports
    I was getting `SingleInstnace is not a constructor` error and it came down to how this was being declared.

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,9 @@
-export declare class SingleInstance {
+export = SingleInstance
+export as namespace SingleInstance;
+
+declare namespace SingleInstance {}
+
+declare class SingleInstance {
 	constructor(appName: string, options?: SingleInstance.SingleInstanceOptions);
 	lock(callback?: () => void): Promise<boolean>
 	unlock(callback?: () => void): Promise<boolean>
